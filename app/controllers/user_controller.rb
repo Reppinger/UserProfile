6e0ae84786b637382cd6b316753e604aff3a323a
Randy Eppinger <RandyEppinger@neo.rr.com>
2013-07-23 15:43:28 -0400

    More refactoring.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,30 +12,34 @@ class UserController < UIViewController
     self.view.backgroundColor = UIColor.whiteColor
     last_label = nil
     User::PROPERTIES.each do |property|
-      label = UILabel.alloc.initWithFrame(CGRectZero)
-      label.text = "#{property.capitalize}:"
-      label.sizeToFit
-      if last_label
-        label.frame = [
-            [last_label.frame.origin.x,
-             last_label.frame.origin.y + last_label.frame.size.height],
-            label.frame.size
-        ]
-      else
-        label.frame = [[10,10], label.frame.size]
-      end
-      last_label = label
-      self.view.addSubview label
+      last_label = create_property_title(last_label, property)
+      self.view.addSubview last_label
       value = UILabel.alloc.initWithFrame(CGRectZero)
       value.text = self.user.send property
       value.sizeToFit
       value.frame = [
-          [label.frame.origin.x + label.frame.size.width + 10,
-           label.frame.origin.y],
+          [last_label.frame.origin.x + last_label.frame.size.width + 10,
+           last_label.frame.origin.y],
           value.frame.size
       ]
       self.view.addSubview value
     end
     self.title = self.user.name
+  end
+
+  def create_property_title(last_label, property)
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = "#{property.capitalize}:"
+    label.sizeToFit
+    if last_label
+      label.frame = [
+          [last_label.frame.origin.x,
+           last_label.frame.origin.y + last_label.frame.size.height],
+          label.frame.size
+      ]
+    else
+      label.frame = [[10, 10], label.frame.size]
+    end
+    label
   end
 end
